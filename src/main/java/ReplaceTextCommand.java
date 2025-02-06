4106ead964a6c1452da7097ef3e135baeaf6080f
Martha Luise Vogel <marthavogel04@gmail.com>
2025-02-06 17:38:28 +0100

    Create ReplaceTextCommand class with method

---
+++ src/main/java/ReplaceTextCommand.java
@@ -0,0 +1,27 @@
+
+public class ReplaceTextCommand implements TextCommand{
+
+  final private String target;
+  final private String replacement;
+
+  public ReplaceTextCommand(String target, String replacement) {
+    this.target = target;
+    this.replacement = replacement;
+  }
+
+  @Override
+  public String execute(String text) {
+    if (text == null || target == null) {
+      return text;
+    }
+    return text.replace(target, replacement);
+  }
+
+  public String getTarget() {
+    return target;
+  }
+
+  public String getReplacement() {
+    return replacement;
+  }
+}
