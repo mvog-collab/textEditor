54ec47d09cd5f4745d5c653bcae8d76a71950f63
Martha Luise Vogel <marthavogel04@gmail.com>
2025-02-06 17:40:51 +0100

    Create ReplaceFirstTextCommand with methods

--- src/main/java/ReplaceFirstTextCommand.java
+++ src/main/java/ReplaceFirstTextCommand.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
+public class ReplaceFirstTextCommand extends ReplaceTextCommand {
 
-public class ReplaceFirstTextCommand {
+  public ReplaceFirstTextCommand(String target, String replacement) {
+    super(target, replacement);
+  }
+
+  @Override
+  public String execute(String text) {
+    if (text == null || getTarget() == null || getReplacement() == null) {
+      return text;
+    }
+    return text.replaceFirst(getTarget(), getReplacement());
+  }
 }
+
