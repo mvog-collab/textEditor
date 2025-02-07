e4f86c4b52ad6144622dc829fc5fd95009d495d0
Martha Luise Vogel <marthavogel04@gmail.com>
2025-02-07 17:20:55 +0100

    Create CapitalizeWordsCommand class wich extends CapitalizeTextCommand

---
+++ src/main/java/capitalizeCommands/CapitalizeWordsCommand.java
@@ -0,0 +1,19 @@
+package capitalizeCommands;
+
+public class CapitalizeWordsCommand extends CapitalizeTextCommand{
+
+  @Override
+  public String execute(String text) {
+    String[] words = text.split("\\s+");
+    StringBuilder result =  new StringBuilder();
+
+    for (String word : words) {
+      if (!word.isEmpty()) {
+        result.append(word.substring(0,1).toUpperCase())
+            .append(word.substring(1))
+            .append(" ");
+      }
+    }
+    return result.toString().trim();
+  }
+}
